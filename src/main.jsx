604a1a515b5d6490787e31719e9ce7adb40279c0
Emin Azeroglu <azeroglu.developer@gmail.com>
2024-08-30 11:52:33 +0400

    fix: Seo Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import "./assets/css/index.css"
 import 'react-toastify/dist/ReactToastify.css';
-import { createRoot } from 'react-dom/client'
-import { RouterProvider, createHashRouter } from "react-router-dom";
+import {createRoot} from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import router from "./router/router";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 import stores from "./stores";
-import { ToastContainer } from 'react-toastify';
+import {ToastContainer} from 'react-toastify';
 
 createRoot(document.getElementById('root')).render(
     <Provider store={stores}>
-        <RouterProvider router={createHashRouter(router)} />
+        <RouterProvider router={createBrowserRouter(router)} />
         <ToastContainer/>
     </Provider>
 )
